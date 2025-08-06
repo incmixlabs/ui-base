8a5fef6aa7726335df7de54e7e8025512b333423
uma-uc <uma.apple123@gmail.com>
2025-08-06 00:01:31 -0700

    feat: wip

--- packages/ui-base/src/components/button.tsx
+++ packages/ui-base/src/components/button.tsx
@@ -1,16 +1,1 @@
-import * as React from 'react';
-import classNames from 'classnames';
-
-import { BaseButton } from './_internal/base-button.js';
-
-type ButtonElement = React.ElementRef<typeof BaseButton>;
-interface ButtonProps extends React.ComponentPropsWithoutRef<typeof BaseButton> {}
-const Button = React.forwardRef<ButtonElement, ButtonProps>(
-  ({ className, ...props }, forwardedRef) => (
-    <BaseButton {...props} ref={forwardedRef} className={classNames('rt-Button', className)} />
-  )
-);
-Button.displayName = 'Button';
-
-export { Button };
-export type { ButtonProps };
+export { Button, type ButtonProps } from '@incmix/radix-ui';
--- packages/ui-base/src/components/portal.tsx
+++ packages/ui-base/src/components/portal.tsx
@@ -1,4 +1,2 @@
-import { Portal as PortalPrimitive } from 'radix-ui';
-export const Portal = PortalPrimitive.Root;
-export const Root = PortalPrimitive.Root;
-export type PortalProps = PortalPrimitive.PortalProps;
+export { Portal } from '@incmix/radix-ui';
+export type {  PortalProps } from '@incmix/radix-ui';
